6552fbc1df6c91dd0db1135a5fb17cd3b9e1fa49
ajithnellikottkrishnadas <ajithnellikodanofficial@gmail.com>
2025-09-23 20:58:05 +0530

    Deploy MERN app to Heroku

--- server/server.js
+++ server/server.js
@@ -6,13 +6,19 @@ import goalRoutes from "./routes/goalRoutes.js";
 import userRoutes from "./routes/userRoutes.js"
 import errorHandler from "./middleware/errorMiddleware.js"
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 connectDB();
 const port = process.env.PORT || 5000;
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const corsOptions = {
-  origin: "http://localhost:3000", // React app
+  origin: "http://localhost:3000",
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
  
 };
@@ -24,6 +30,13 @@ app.use(express.urlencoded({ extended: false })); // middleware bodyparser to ge
 
 app.use("/api/goals", goalRoutes);
 app.use("/api/users", userRoutes)
+
+if(process.env.NODE_ENV=== "production"){
+    app.use(express.static(path.join(__dirname,"../client/build")))
+    app.get(/.*/, (req,res)=> res.sendFile(path.resolve(__dirname,"../client", "build", "index.html")))
+}else{
+  app.get("/", (req,res)=> res.send("please set to production"))
+}
 
 app.use(errorHandler)
 
